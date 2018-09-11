6a9151acb105700a8c5d88b1be7a24c2673b88b8
drlwjy <drl@wjy.com>
2018-09-11 22:08:47 +0800

    init

--- cellrobot_cluster_maml_trpo.py
+++ cellrobot_cluster_maml_trpo.py
@@ -8,10 +8,17 @@ from rllab.envs.normalized_env import normalize
 from rllab.misc.instrument import stub, run_experiment_lite
 from sandbox.rocky.tf.policies.maml_minimal_gauss_mlp_policy import MAMLGaussianMLPPolicy
 
-#from sandbox.rocky.tf.policies.maml_minimal_gauss_mlp_policy_2 import MAMLGaussianMLPPolicy
+# from sandbox.rocky.tf.policies.maml_minimal_gauss_mlp_policy_2 import MAMLGaussianMLPPolicy
 from sandbox.rocky.tf.envs.base import TfEnv
 
 import tensorflow as tf
+
+import os
+from datetime import datetime
+import shutil
+import glob
+
+import vendor.ssh as ssh
 
 stub(globals())
 
@@ -19,45 +26,59 @@ from rllab.misc.instrument import VariantGenerator, variant
 
 
 class VG(VariantGenerator):
-
+    
     @variant
     def fast_lr(self):
         return [0.1]
-
+    
     @variant
     def meta_step_size(self):
-        return [0.01] # sometimes 0.02 better
-
+        return [0.01, 0.02]  # sometimes 0.02 better
+    
     @variant
     def fast_batch_size(self):
         return [20]
-
+    
     @variant
     def meta_batch_size(self):
-        return [40] # at least a total batch size of 400. (meta batch size*fast batch size)
-
+        return [20]  # at least a total batch size of 400. (meta batch size*fast batch size)
+    
     @variant
     def seed(self):
         return [1]
-
+    
     @variant
     def task_var(self):  # fwd/bwd task or goal vel task
         # 0 for fwd/bwd, 1 for goal vel (kind of), 2 for goal pose
         return [1]
 
 
+ssh_FLAG = True
+
+exp_id = 1
+variants = VG().variants()
+num = 0
+for v in variants:
+    num += 1
+    print('exp{}: '.format(num), v)
+
+# SSH Config
+hostname = '2402:f000:6:3801:ee1c:d67d:4f92:55ad'  # '2600:1f16:e7a:a088:805d:16d6:f387:62e5'
+username = 'drl'
+key_path = '/home/ubuntu/.ssh/id_rsa_dl'
+
+port = 22
 # should also code up alternative KL thing
 
 variants = VG().variants()
 
-max_path_length = 200
+max_path_length = 500
 num_grad_updates = 1
-use_maml=True
-exp_id = 1
+use_maml = True
 
 for v in variants:
     task_var = v['task_var']
-
+    
     if task_var == 0:
         env = TfEnv(normalize(AntEnvRandDirec()))
         task_var = 'lalalala'
@@ -73,16 +94,16 @@ for v in variants:
         grad_step_size=v['fast_lr'],
         hidden_nonlinearity=tf.nn.relu,
         output_nonlinearity=tf.nn.sigmoid,
-        hidden_sizes=(64,64),
+        hidden_sizes=(64, 64),
     
     )
-
+    
     baseline = LinearFeatureBaseline(env_spec=env.spec)
     algo = MAMLTRPO(
         env=env,
         policy=policy,
         baseline=baseline,
-        batch_size=v['fast_batch_size'], # number of trajs for grad update
+        batch_size=v['fast_batch_size'],  # number of trajs for grad update
         max_path_length=max_path_length,
         meta_batch_size=v['meta_batch_size'],
         num_grad_updates=num_grad_updates,
@@ -91,11 +112,12 @@ for v in variants:
         step_size=v['meta_step_size'],
         plot=False,
     )
-
+    exp_name = 'Cellrobot_trpo_maml' + task_var + '_' + str(max_path_length) + '_EXP' + str(exp_id)
     run_experiment_lite(
         algo.train(),
-        exp_prefix='Cellrobot_trpo_maml' + task_var + '_' + str(max_path_length) ,
-        exp_name='maml'+str(int(use_maml))+'_fbs'+str(v['fast_batch_size'])+'_mbs'+str(v['meta_batch_size'])+'_flr_' + str(v['fast_lr'])  + '_mlr' + str(v['meta_step_size'])+'_EXP'+str(exp_id),
+        exp_prefix=exp_name,
+        exp_name='maml' + str(int(use_maml)) + '_fbs' + str(v['fast_batch_size']) + '_mbs' + str(
+            v['meta_batch_size']) + '_flr_' + str(v['fast_lr']) + '_mlr' + str(v['meta_step_size']),
         # Number of parallel workers for sampling
         n_parallel=71,
         # Only keep the snapshot parameters for the last iteration
@@ -106,8 +128,14 @@ for v in variants:
         # will be used
         seed=v["seed"],
         mode="local",
-        #mode="ec2",
+        # mode="ec2",
         variant=v,
         # plot=True,
         # terminate_machine=False,
     )
+    
+    if ssh_FLAG:
+        local_dir = os.path.abspath('data/local/' + exp_name + '/')
+        remote_dir = '/home/drl/PycharmProjects/DeployedProjects/CR_CPG/Hyper_lab/log-files/AWS_logfiles/' + exp_name + '/'
+        ssh.upload(local_dir, remote_dir, hostname=hostname, port=port, username=username,
+                   pkey_path=key_path)
