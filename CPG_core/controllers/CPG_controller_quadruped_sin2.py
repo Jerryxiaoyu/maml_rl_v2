79d3dd12dbdb8a725cbd9177489da7cafeb1194a
drlwjy <drl@wjy.com>
2018-09-14 12:04:46 +0800

    EXP8

---
+++ CPG_core/controllers/CPG_controller_quadruped_sin2.py
@@ -0,0 +1,244 @@
+from CPG_core.CPG_Sin_osillator import sin_oscillator
+
+
+from CPG_core.CPG_Sin_osillator import CPG_Sinneutron
+class CPG_network(object):
+    def __init__(self, position_vector):
+        kf = position_vector[0]
+        GAIN0 = position_vector[1]
+        GAIN1 = position_vector[2]
+        GAIN2 = position_vector[3]
+        GAIN3 = position_vector[4]
+        GAIN4 = position_vector[5]
+        GAIN5 = position_vector[6]
+        GAIN6 = position_vector[7]
+        GAIN7 = position_vector[8]
+        GAIN8 = position_vector[9]
+        GAIN9 = position_vector[10]
+        GAIN10 = position_vector[11]
+        GAIN11 = position_vector[12]
+        GAIN12 = position_vector[13]
+    
+        BIAS0 = position_vector[14]
+        BIAS1 = position_vector[15]
+        BIAS2 = position_vector[16]
+        BIAS3 = position_vector[17]
+        BIAS4 = position_vector[18]
+        BIAS5 = position_vector[19]
+        BIAS6 = position_vector[20]
+        BIAS7 = position_vector[21]
+        BIAS8 = position_vector[22]
+        BIAS9 = position_vector[23]
+        BIAS10 = position_vector[24]
+        BIAS11 = position_vector[25]
+        BIAS12 = position_vector[26]
+
+        PHASE0 = position_vector[27]
+        PHASE1 = position_vector[28]
+        PHASE2 = position_vector[29]
+        PHASE3 = position_vector[30]
+        PHASE4 = position_vector[31]
+        PHASE5 = position_vector[32]
+        PHASE6 = position_vector[33]
+        PHASE7 = position_vector[34]
+        PHASE8 = position_vector[35]
+        PHASE9 = position_vector[36]
+        PHASE10 = position_vector[37]
+        PHASE11 = position_vector[38]
+        PHASE12 = position_vector[39]
+
+        self.parm_list = {
+            0:  [0.0, 0.0, 0.0,    1.0, 0.0, 0],
+            1: [0.0, 0.0, 0.0,    GAIN0, BIAS0, PHASE0],
+            2: [0.0, 0.0, 0.0,    GAIN1, BIAS1, PHASE1],
+            3: [0.0, 0.0, 0.0,    GAIN2, BIAS2, PHASE2],
+            4: [0.0, 0.0, 0.0,     GAIN3, BIAS3, PHASE3],
+            5: [0.0, 0.0, 0.0,     GAIN4, BIAS4, PHASE4],
+            6: [0.0, 0.0, 0.0,  GAIN5, BIAS5, PHASE5],
+            7: [0.0, 0.0, 0.0,    GAIN6, BIAS6, PHASE6],
+            8: [0.0, 0.0, 0.0,   GAIN7, BIAS7, PHASE7],
+            9: [0.0, 0.0, 0.0,  GAIN8, BIAS8, PHASE8],
+            10: [0.0, 0.0, 0.0,   GAIN9, BIAS9, PHASE9],
+            11: [0.0, 0.0, 0.0,  GAIN10, BIAS10, PHASE10],
+            12: [0.0, 0.0, 0.0,    GAIN11, BIAS11, PHASE11],
+            13: [0.0, 0.0, 0.0,   GAIN12, BIAS12, PHASE12],
+        }
+        
+        self.kf = position_vector[0]
+        self.num_CPG = len(self.parm_list)
+        self.CPG_list =[]
+        
+        self.w_ms_list = [None, 1, 1, -1, - 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  #yuanlai hao de
+        #self.w_ms_list =[None, 1, -1, -1, -1, -1, 1, 1, -1, -1, -1, 1, 1, 1]
+        
+        self.master_list = [None,  0,1,1,1,1,2,2,3,3,4,4,5,5]
+        
+        
+        for i in range(self.num_CPG):
+            if i == 0:
+                self.CPG_list.append(CPG_Sinneutron(0, master_nuron = None, param=self.parm_list[0] ,kf= self.kf, w_ms = 0))
+            else:
+                self.CPG_list.append(CPG_Sinneutron(i, master_nuron=self.CPG_list[self.master_list[i]],
+                                                 param=self.parm_list[i], kf=self.kf, w_ms= self.w_ms_list[i]))
+                
+        #print('CPG init')
+    
+    def output(self, state):
+        output_list = []
+        for cpg_n in self.CPG_list:
+            cpg_n.next_output(f1=0, f2=0)
+            output_list.append(cpg_n.parm['o'])
+        
+        return output_list
+
+    def update(self, fi_l):
+    
+        if len(fi_l) ==2:
+            
+            # f_left = 1 - (0.5 - fi_l[0]) * self.kesi
+            # f_right = 1 - (0.5 - fi_l[1]) * self.kesi
+            # self.CPG_list[2].parm['f12'] = self.parm_list[2][5] * f_left
+            # self.CPG_list[6].parm['f12'] = self.parm_list[6][5] * f_left
+            # self.CPG_list[7].parm['f12'] = self.parm_list[7][5] * f_left
+            #
+            # self.CPG_list[3].parm['f12'] = self.parm_list[3][5] * f_right
+            # self.CPG_list[8].parm['f12'] = self.parm_list[8][5] * f_right
+            # self.CPG_list[9].parm['f12'] = self.parm_list[9][5] * f_right
+            #
+            # self.CPG_list[4].parm['f12'] = self.parm_list[4][5] * f_right
+            # self.CPG_list[10].parm['f12'] = self.parm_list[10][5] * f_right
+            # self.CPG_list[11].parm['f12'] = self.parm_list[11][5] * f_right
+            #
+            # self.CPG_list[5].parm['f12'] = self.parm_list[5][5] * f_left
+            # self.CPG_list[12].parm['f12'] = self.parm_list[12][5] * f_left
+            # self.CPG_list[13].parm['f12'] = self.parm_list[13][5] * f_left
+            self.kesi =1.0
+            gain_left =  (1 - 3*fi_l[0]) * self.kesi
+            gain_right =  (1 - 3*fi_l[1]) * self.kesi
+            
+            
+           # print('gain =', gain_left,gain_right)
+            
+            self.CPG_list[2].parm['R1'] = self.parm_list[2][3] * gain_left
+            self.CPG_list[6].parm['R1'] = self.parm_list[6][3] * gain_left
+            self.CPG_list[7].parm['R1'] = self.parm_list[7][3] * gain_left
+
+            self.CPG_list[3].parm['R1'] = self.parm_list[3][3] * gain_left
+            self.CPG_list[8].parm['R1'] = self.parm_list[8][3] * gain_left
+            self.CPG_list[9].parm['R1'] = self.parm_list[9][3] * gain_left
+
+            self.CPG_list[4].parm['R1'] = self.parm_list[4][3] * gain_right
+            self.CPG_list[10].parm['R1'] = self.parm_list[10][3] * gain_right
+            self.CPG_list[11].parm['R1'] = self.parm_list[11][3] * gain_right
+
+            self.CPG_list[5].parm['R1'] = self.parm_list[5][3] * gain_right
+            self.CPG_list[12].parm['R1'] = self.parm_list[12][3] * gain_right
+            self.CPG_list[13].parm['R1'] = self.parm_list[13][3] * gain_right
+            
+        elif len(fi_l) ==4:
+
+            f_left = 1 - (0.5 - fi_l[0]) * self.kesi
+            f_right = 1 - (0.5 - fi_l[1] ) * self.kesi
+            gain_left= 1 - (0.5 - fi_l[2]) * self.kesi
+            gain_right = 1 - (0.5 -fi_l[3] ) * self.kesi
+
+            self.CPG_list[2].parm['f12'] = self.parm_list[2][5] * f_left
+            self.CPG_list[6].parm['f12'] = self.parm_list[6][5] * f_left
+            self.CPG_list[7].parm['f12'] = self.parm_list[7][5] * f_left
+
+            self.CPG_list[3].parm['f12'] = self.parm_list[3][5] * f_right
+            self.CPG_list[8].parm['f12'] = self.parm_list[8][5] * f_right
+            self.CPG_list[9].parm['f12'] = self.parm_list[9][5] * f_right
+
+            self.CPG_list[4].parm['f12'] = self.parm_list[4][5] * f_right
+            self.CPG_list[10].parm['f12'] = self.parm_list[10][5] * f_right
+            self.CPG_list[11].parm['f12'] = self.parm_list[11][5] * f_right
+
+            self.CPG_list[5].parm['f12'] = self.parm_list[5][5] * f_left
+            self.CPG_list[12].parm['f12'] = self.parm_list[12][5] * f_left
+            self.CPG_list[13].parm['f12'] = self.parm_list[13][5] * f_left
+#           #gain
+            self.CPG_list[2].parm['R1'] = self.parm_list[2][3] * gain_left
+            self.CPG_list[6].parm['R1'] = self.parm_list[6][3] * gain_left
+            self.CPG_list[7].parm['R1'] = self.parm_list[7][3] * gain_left
+
+            self.CPG_list[3].parm['R1'] = self.parm_list[3][3] * gain_right
+            self.CPG_list[8].parm['R1'] = self.parm_list[8][3] * gain_right
+            self.CPG_list[9].parm['R1'] = self.parm_list[9][3] * gain_right
+
+            self.CPG_list[4].parm['R1'] = self.parm_list[4][3] * gain_right
+            self.CPG_list[10].parm['R1'] = self.parm_list[10][3] * gain_right
+            self.CPG_list[11].parm['R1'] = self.parm_list[11][3] * gain_right
+
+            self.CPG_list[5].parm['R1'] = self.parm_list[5][3] * gain_left
+            self.CPG_list[12].parm['R1'] = self.parm_list[12][3] * gain_left
+            self.CPG_list[13].parm['R1'] = self.parm_list[13][3] * gain_left
+            
+    
+        # if len(fi_l) == 4:
+        #     print('add gain parm')
+        #     fl_gain_left = 1 - (1.0 - self.kesi) * fi_l[2]
+        #     fl_gain_right = 1 - (1.0 - self.kesi) * fi_l[3]
+        #
+        #     self.CPG_list[2].parm['R1'] = self.parm_list[2][3] * fl_gain_left
+        #     self.CPG_list[6].parm['R1'] = self.parm_list[6][3] * fl_gain_left
+        #     self.CPG_list[7].parm['R1'] = self.parm_list[7][3] * fl_gain_left
+        #
+        #     self.CPG_list[3].parm['R1'] = self.parm_list[3][3] * fl_gain_right
+        #     self.CPG_list[8].parm['R1'] = self.parm_list[8][3] * fl_gain_right
+        #     self.CPG_list[9].parm['R1'] = self.parm_list[9][3] * fl_gain_right
+        #
+        #     self.CPG_list[4].parm['R1'] = self.parm_list[4][3] * fl_gain_right
+        #     self.CPG_list[10].parm['R1'] = self.parm_list[10][3] * fl_gain_right
+        #     self.CPG_list[11].parm['R1'] = self.parm_list[11][3] * fl_gain_right
+        #
+        #     self.CPG_list[5].parm['R1'] = self.parm_list[5][3] * fl_gain_left
+        #     self.CPG_list[12].parm['R1'] = self.parm_list[12][3] * fl_gain_left
+        #     self.CPG_list[13].parm['R1'] = self.parm_list[13][3] * fl_gain_left
+
+
+class CPG_network5(object):
+    def __init__(self, CPG_node_num,position_vector):
+        
+        self.CPG_node_num = CPG_node_num  # 不包括placemarker
+
+        if len(position_vector) != self.CPG_node_num * 4 + 1:
+            assert "Position vector out of range!"
+
+        GAIN, BIAS, PHASE, WEIGHT = [], [], [], []
+
+        for i in range(self.CPG_node_num):
+            GAIN.append(position_vector[i + 1])
+            BIAS.append(position_vector[self.CPG_node_num + i + 1])
+            PHASE.append(position_vector[2 * self.CPG_node_num + i + 1])
+            WEIGHT.append(position_vector[3 * self.CPG_node_num + i + 1])
+
+        parm_list = {
+            0: [0.0, 0.0, 0.0, 1.0, 0.0, 0],
+        }
+        for i in range(self.CPG_node_num):
+            parm = {i + 1: [0.0, 0.0, 0.0, GAIN[i], BIAS[i], PHASE[i]]}
+            parm_list.update(parm)
+        
+        self.kf = position_vector[0]
+        self.num_CPG = len(parm_list)
+        self.CPG_list = []
+        self.w_ms_list = [None, WEIGHT[0], WEIGHT[1], WEIGHT[2], WEIGHT[3], WEIGHT[4], WEIGHT[5], WEIGHT[6], WEIGHT[7], WEIGHT[8],WEIGHT[9], WEIGHT[10], WEIGHT[11], WEIGHT[12],]
+        self.master_list = [None, 0, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+        
+        for i in range(self.num_CPG):
+            if i == 0:
+                self.CPG_list.append(CPG_Sinneutron(0, master_nuron=None, param=parm_list[0], kf=self.kf, w_ms=0))
+            else:
+                self.CPG_list.append(CPG_Sinneutron(i, master_nuron=self.CPG_list[self.master_list[i]],
+                                                    param=parm_list[i], kf=self.kf, w_ms=self.w_ms_list[i]))
+    
+    def output(self, state):
+        output_list = []
+        for cpg_n in self.CPG_list:
+            cpg_n.next_output(f1=0, f2=0)
+            output_list.append(cpg_n.parm['o'])
+        
+        return output_list
+        
+       
---
+++ cellrobot_cluster_maml_trpo_new.py
@@ -0,0 +1,148 @@
+from sandbox.rocky.tf.algos.maml_trpo import MAMLTRPO
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.baselines.gaussian_mlp_baseline import GaussianMLPBaseline
+from rllab.envs.mujoco.cellrobot_rand_direc_env import CellRobotRandDirectEnv
+from rllab.envs.mujoco.cellrobot_rand_direc2_env import CellRobotRandDirect2Env
+from rllab.envs.mujoco.cellrobot_rand_direc_pi4_env import CellRobotRandDirectpi4Env
+from rllab.envs.mujoco.cellrobot_rand_direc_pi4_env2 import CellRobotRandDirectpi4Env2
+from rllab.envs.mujoco.cellrobot_rand_direc_env_body import CellRobotRandDirectBodyEnv
+from rllab.envs.mujoco.cellrobot_rand_direc_env_body_new import CellRobotRandDirectBodyNewEnv
+from rllab.envs.mujoco.ant_env_rand_goal import AntEnvRandGoal
+from rllab.envs.mujoco.ant_env_rand_direc import AntEnvRandDirec
+from rllab.envs.normalized_env import normalize
+from rllab.misc.instrument import stub, run_experiment_lite
+from sandbox.rocky.tf.policies.maml_minimal_gauss_mlp_policy import MAMLGaussianMLPPolicy
+
+# from sandbox.rocky.tf.policies.maml_minimal_gauss_mlp_policy_2 import MAMLGaussianMLPPolicy
+from sandbox.rocky.tf.envs.base import TfEnv
+
+import tensorflow as tf
+
+import os
+from datetime import datetime
+import shutil
+import glob
+
+import vendor.ssh as ssh
+
+stub(globals())
+
+from rllab.misc.instrument import VariantGenerator, variant
+
+
+class VG(VariantGenerator):
+    
+    @variant
+    def fast_lr(self):
+        return [0.1]
+    
+    @variant
+    def meta_step_size(self):
+        return [0.02,0.05, ]  # sometimes 0.02 better
+    
+    @variant
+    def fast_batch_size(self):
+        return [20]
+    
+    @variant
+    def meta_batch_size(self):
+        return [20]  # at least a total batch size of 400. (meta batch size*fast batch size)
+    
+    @variant
+    def seed(self):
+        return [1]
+    
+    @variant
+    def task_var(self):  # fwd/bwd task or goal vel task
+        # 0 for fwd/bwd, 1 for goal vel (kind of), 2 for goal pose
+        return [0]
+
+
+ssh_FLAG = False
+
+exp_id = 10
+variants = VG().variants()
+num = 0
+for v in variants:
+    num += 1
+    print('exp{}: '.format(num), v)
+
+# SSH Config
+hostname = '2402:f000:6:3801:ee1c:d67d:4f92:55ad'  # '2600:1f16:e7a:a088:805d:16d6:f387:62e5'
+username = 'drl'
+key_path = '/home/ubuntu/.ssh/id_rsa_dl'
+
+port = 22
+# should also code up alternative KL thing
+
+variants = VG().variants()
+
+max_path_length = 500
+num_grad_updates = 1
+use_maml = True
+
+for v in variants:
+    task_var = v['task_var']
+    
+    if task_var == 0:
+        env = TfEnv(normalize(CellRobotRandDirectBodyNewEnv()))
+        task_var = 'direct_new'
+     
+    exp_name = 'Cellrobot_trpo_maml' + str(task_var) + '_' + str(max_path_length) + '_EXP' + str(exp_id)
+
+    filenames = glob.glob('*.py')  # put copy of all python files in log_dir
+    for filename in filenames:  # for reference
+        shutil.copy(filename, os.path.join('data/local', exp_name))
+    
+    policy = MAMLGaussianMLPPolicy(
+        name="policy",
+        env_spec=env.spec,
+        grad_step_size=v['fast_lr'],
+        hidden_nonlinearity=tf.nn.relu,
+        output_nonlinearity=tf.nn.tanh,
+        hidden_sizes=(64, 64),
+    
+    )
+    
+    baseline = LinearFeatureBaseline(env_spec=env.spec)
+    algo = MAMLTRPO(
+        env=env,
+        policy=policy,
+        baseline=baseline,
+        batch_size=v['fast_batch_size'],  # number of trajs for grad update
+        max_path_length=max_path_length,
+        meta_batch_size=v['meta_batch_size'],
+        num_grad_updates=num_grad_updates,
+        n_itr=800,
+        use_maml=use_maml,
+        step_size=v['meta_step_size'],
+        plot=False,
+    )
+    
+    run_experiment_lite(
+        algo.train(),
+        exp_prefix=exp_name,
+        exp_name='maml' + str(int(use_maml)) + '_fbs' + str(v['fast_batch_size']) + '_mbs' + str(
+            v['meta_batch_size']) + '_flr_' + str(v['fast_lr']) + '_mlr' + str(v['meta_step_size']),
+        # Number of parallel workers for sampling
+        n_parallel=2,
+        # Only keep the snapshot parameters for the last iteration
+        snapshot_mode="gap",
+        snapshot_gap=2,
+        sync_s3_pkl=True,
+        # Specifies the seed for the experiment. If this is not provided, a random seed
+        # will be used
+        seed=v["seed"],
+        mode="local",
+        # mode="ec2",
+        use_gpu=False,
+        variant=v,
+        # plot=True,
+        # terminate_machine=False,
+    )
+    
+    if ssh_FLAG:
+        local_dir = os.path.abspath('data/local/' + exp_name + '/')
+        remote_dir = '/home/drl/PycharmProjects/DeployedProjects/CR_CPG/Hyper_lab/log-files/AWS_logfiles/' + exp_name + '/'
+        ssh.upload(local_dir, remote_dir, hostname=hostname, port=port, username=username,
+                   pkey_path=key_path)
